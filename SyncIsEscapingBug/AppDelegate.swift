ede40cfc7bb1c4111732a5f7b3aa2f6cd3d4cbe9
Robert Ryan <robert.ryan@mindspring.com>
2020-07-01 11:51:20 -0700

    Initial Commit

---
+++ SyncIsEscapingBug/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SyncIsEscapingBug
+//
+//  Created by Robert Ryan on 7/1/20.
+//  Copyright © 2020 Robert Ryan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SyncIsEscapingBug/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SyncIsEscapingBug
+//
+//  Created by Robert Ryan on 7/1/20.
+//  Copyright © 2020 Robert Ryan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
